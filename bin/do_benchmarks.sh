4389a6a822ab78a5b8b09ddabd8c29d7b33f9766
LuYufei(yuwen) <yuwen.lyf@alibaba-inc.com>
2021-09-26 10:22:11 +0800

    update bin/do_benchmarks.sh params

--- bin/do_benchmarks.sh
+++ bin/do_benchmarks.sh
@@ -1,19 +1,20 @@
-./PSImain.cpp_exe -n 4096 -m 2097151 -b 21 -L 450 2>&1 | tee n4096_m2097153_b21_L450.txt &&
-./PSImain.cpp_exe -n 1024 -m 2097151 -b 21 -L 450 2>&1 | tee n1024_m2097153_b21_L450.txt &&
-./PSImain.cpp_exe -n 8192 -m 2097151 -b 21 -L 450 2>&1 | tee n8192_m2097153_b21_L450.txt &&
-./PSImain.cpp_exe -n 8192 -m 65535 -b 16 -L 300 2>&1 | tee n8192_m65535_b16_L300.txt &&
+./PSImain.cpp_exe -n 4096 -m 2097150 -b 21 -L 450 2>&1 | tee n4096_m2097153_b21_L450.txt &&
+./PSImain.cpp_exe -n 1024 -m 2097150 -b 21 -L 450 2>&1 | tee n1024_m2097153_b21_L450.txt &&
+./PSImain.cpp_exe -n 8192 -m 2097150 -b 21 -L 450 2>&1 | tee n8192_m2097153_b21_L450.txt &&
+./PSImain.cpp_exe -n 8192 -m 65534 -b 16 -L 300 2>&1 | tee n8192_m65535_b16_L300.txt &&
 ./PSImain.cpp_exe -n 8192 -m 32767 -b 15 -L 300 2>&1 | tee n8192_m32767_b15_L300.txt &&
 
-./PSImain.cpp_exe -n 8192 -m 4095 -b 12 -L 300 2>&1 | tee n8192_m4095_b12_L300.txt &&
-./PSImain.cpp_exe -n 8192 -m 8191 -b 13 -L 300 2>&1 | tee n8192_m8191_b13_L300.txt &&
-./PSImain.cpp_exe -n 8192 -m 16383 -b 14 -L 300 2>&1 | tee n8192_m16383_b14_L300.txt &&
-./PSImain.cpp_exe -n 8192 -m 131071 -b 17 -L 450 2>&1 | tee n8192_m131071_b17_L450.txt &&
-./PSImain.cpp_exe -n 8192 -m 262143 -b 18 -L 450 2>&1 | tee n8192_m262143_b18_L450.txt &&
-./PSImain.cpp_exe -n 8192 -m 524287 -b 19 -L 450 2>&1 | tee n8192_m524287_b19_L450.txt &&
-./PSImain.cpp_exe -n 8192 -m 1048575 -b 20 -L 450 2>&1 | tee n8192_m1048575_b20_L450.txt &&
+./PSImain.cpp_exe -n 8192 -m 4094 -b 12 -L 300 2>&1 | tee n8192_m4095_b12_L300.txt &&
+./PSImain.cpp_exe -n 8192 -m 8190 -b 13 -L 300 2>&1 | tee n8192_m8191_b13_L300.txt &&
+./PSImain.cpp_exe -n 8192 -m 16382 -b 14 -L 300 2>&1 | tee n8192_m16383_b14_L300.txt &&
+./PSImain.cpp_exe -n 8192 -m 131070 -b 17 -L 450 2>&1 | tee n8192_m131071_b17_L450.txt &&
+./PSImain.cpp_exe -n 8192 -m 262142 -b 18 -L 450 2>&1 | tee n8192_m262143_b18_L450.txt &&
+./PSImain.cpp_exe -n 8192 -m 524286 -b 19 -L 450 2>&1 | tee n8192_m524287_b19_L450.txt &&
+./PSImain.cpp_exe -n 8192 -m 1048574 -b 20 -L 450 2>&1 | tee n8192_m1048575_b20_L450.txt &&
 
 
-./PSImain.cpp_exe -n 4096 -m 1048575 -b 20 -L 450 2>&1 | tee n4096_m1048575_b20_L450.txt &&
-./PSImain.cpp_exe -n 4096 -m 524287 -b 19 -L 450 2>&1 | tee n4096_m524287_b19_L450.txt &&
-./PSImain.cpp_exe -n 4096 -m 262143 -b 18 -L 450 2>&1 | tee n4096_m262144_b18_L450.txt &&
-./PSImain.cpp_exe -n 4096 -m 131071 -b 17 -L 450 2>&1 | tee n4096_m131071_b17_L450.txt 
+./PSImain.cpp_exe -n 4096 -m 1048574 -b 20 -L 450 2>&1 | tee n4096_m1048575_b20_L450.txt &&
+./PSImain.cpp_exe -n 4096 -m 524286 -b 19 -L 450 2>&1 | tee n4096_m524287_b19_L450.txt &&
+./PSImain.cpp_exe -n 4096 -m 262142 -b 18 -L 450 2>&1 | tee n4096_m262144_b18_L450.txt &&
+./PSImain.cpp_exe -n 4096 -m 131070 -b 17 -L 450 2>&1 | tee n4096_m131071_b17_L450.txt 
+
